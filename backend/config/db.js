59be2fc96ed17c8e8ef734fafcc25b5f493a402c
prayagbit75 <prayagkirar30081999@gmail.com>
2026-03-08 13:46:46 +0530

    Update database password in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "mysqlprayag",
+  password: "Mysql@123",
   database: "mysql_db",
   waitForConnections: true,
   connectionLimit: 10,
